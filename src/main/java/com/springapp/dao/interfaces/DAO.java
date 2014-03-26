3642501d3caf3133d20a7f605ce4e03a79dd9bd7
aurdo <aurdo.0@gmail.com>
2014-03-26 02:16:02 +0200

    DAO interface draft. That's interface for Factory (would be renamed to HibernateDAO)

---
+++ src/main/java/com/springapp/dao/interfaces/DAO.java
@@ -0,0 +1,12 @@
+package com.springapp.dao.interfaces;
+
+/**
+ * Created by Alex on 3/26/14.
+ */
+public interface DAO {
+    public DAOCategory DaoCategory();
+
+    public DAOProduct DaoProduct();
+
+    public DAOUser DAOUser();
+}
