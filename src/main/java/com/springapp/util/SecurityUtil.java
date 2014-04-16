e069ad3fce535070acb44374768897ce19a3b489
aurdo <aurdo.0@gmail.com>
2014-04-16 20:18:48 +0300

    MD5 hash is now calculated. Maybe, there is a reason to use spring-security library for that.

--- src/main/java/com/springapp/util/SecurityUtil.java
+++ src/main/java/com/springapp/util/SecurityUtil.java
@@ -1,5 +1,6 @@
 package com.springapp.util;
 
+import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -9,11 +10,12 @@ import java.security.NoSuchAlgorithmException;
 public class SecurityUtil {
 
     public static String md5(String input) {
-        byte[] bytes = input.getBytes();
+        //byte[] bytes = input.getBytes();
         try {
             MessageDigest digest = MessageDigest.getInstance("md5");
+            digest.update(input.getBytes(), 0, input.length());
             //TODO:Conversion to string
-            return digest.digest(bytes).toString();
+            return String.format("%032X", new BigInteger(1, digest.digest()));
 
         } catch (NoSuchAlgorithmException e) {
             //TODO:Throw exception
