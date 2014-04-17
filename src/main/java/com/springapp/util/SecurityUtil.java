ee38cc5d9693e0aae9eb22f313526990c11caadb
aurdo <aurdo.0@gmail.com>
2014-04-17 16:59:53 +0300

    MD5(MD5(string)+salt) added

--- src/main/java/com/springapp/util/SecurityUtil.java
+++ src/main/java/com/springapp/util/SecurityUtil.java
@@ -3,6 +3,8 @@ package com.springapp.util;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by aurdo on 04/03/14.
@@ -11,16 +13,21 @@ public class SecurityUtil {
 
     public static String md5(String input) {
         //byte[] bytes = input.getBytes();
+        Logger logger = Logger.getLogger(SecurityUtil.class.getName());
         try {
             MessageDigest digest = MessageDigest.getInstance("md5");
             digest.update(input.getBytes(), 0, input.length());
-            //TODO:Conversion to string
             return String.format("%032X", new BigInteger(1, digest.digest()));
 
         } catch (NoSuchAlgorithmException e) {
-            //TODO:Throw exception
+            logger.log(Level.SEVERE, "Hash generation error", e);
+            //Todo:throw exception or check for zero-string at the upper level
         }
         return "";
+    }
+
+    public static String md5(String input, String salt) {
+        return md5(md5(input) + salt);
     }
 
 
